c518d18122ef0c3e100b5e87eb32310f342b8c54
Bo Adler <thumper@alumni.caltech.edu>
2011-01-18 16:29:06 -0800

    instructions: how to click on a Mac

--- src/chrome/content/wikitrust.js
+++ src/chrome/content/wikitrust.js
@@ -3,7 +3,7 @@
 (function() {
   const langmsgs = {
 	en: { success: '<table border="1" cellpadding="5" cellspacing="0" style="background:lightgreen; color:black; margin-top: 10px; margin-bottom: 10px;" id="wt-expl">'
-			+ '<tr><td>The article text is colored according to how much it has been revised.  An orange background indicates new, unrevised, text;  white is for text that has been revised by many reputed authors.  If you CTRL-ALT-click on a word, you will be redirected to the diff corresponding to the edit where the word was introduced.</td></tr>'
+			+ '<tr><td>The article text is colored according to how much it has been revised.  An orange background indicates new, unrevised, text;  white is for text that has been revised by many reputed authors.  If you CTRL-ALT-click on a word (use CMD-ALT on a Mac), you will be redirected to the diff corresponding to the edit where the word was introduced.</td></tr>'
 			+ '<tr><td>The text color and origin are computed by <a href="http://wikitrust.soe.ucsc.edu/" class="external text" title="http://wikitrust.soe.ucsc.edu/" rel="nofollow">WikiTrust</a>; if you notice problems, you can submit a bug report <a href="http://code.google.com/p/wikitrust/issues" class="external text" title="http://code.google.com/p/wikitrust/issues" rel="nofollow">here</a>.</td></tr>'
 			+ '</table>',
 			tabhover:  'Trust colored version of this page',
